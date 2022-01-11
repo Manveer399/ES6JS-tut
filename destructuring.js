67a0d264e21f8343113dc17e3b59133a8862bf7d
Manveer Kasseran <Manveer399@users.noreply.github.com>
2022-01-11 23:29:10 +0800

    array destructuring

---
+++ destructuring.js
@@ -0,0 +1,34 @@
+//Assign variable from Arrays
+/*
+const alphabet = ['A','B','C','D','E','F'];
+const numbers = ['1','2','3','4','5','6']
+
+const [a,b] = alphabet
+const [a,,c] = alphabet
+const [a,b,c,...rest] = alphabet
+
+const newArray = alphabet.concat(numbers);
+const newArray2 = [...alphabet,...numbers];
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(rest);
+console.log(newArray);
+
+example of its use 
+
+function sumAndMultiply (a,b) {
+  return [a+b,a*b]
+}
+
+const array = sumAndMultiply(2,3)
+
+const [sum,multiply] = sumAndMultiply(2,3);
+
+const [sum,multiply, division = 'No division'] = sumAndMultiply(2,3);
+
+*/
+
+
+/*Objects destructuring*/
+
